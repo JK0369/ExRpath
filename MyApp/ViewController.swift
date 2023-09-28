6d60251e44916c0e9fdfc814b3f795a132e87650
jake <palatable7@naver.com>
2023-09-28 15:31:24 +0900

    Implement

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by 김종권 on 2023/09/28.
+//
+
+import UIKit
+import Jake
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let string = JakeString()
+        print(string.name)
+    }
+}
+
